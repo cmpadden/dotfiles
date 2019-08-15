f4c071fcdd23b88713eb5109523f4b03e9c2d744
Colton Padden <cp1060@georgetown.edu>
2019-08-15 10:27:54 -0400

    fixed shellcheck warnings for aliases

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -1,12 +1,30 @@
+# Docker
 alias drmf='docker rmi -f $(docker images -q)'
 
+# Git
 alias gs="git status"
 alias gu="git shortlog | grep -E '^[^ ]'"
 
-alias l='ls -lhp'
-alias ls='ls -p'
+# https://github.com/ogham/exa
+if hash exa 2>/dev/null; then
+  alias l='exa -1'
+else
+  alias l='ls -lhp'
+fi
 
-alias n="cd ${HOME}/workspace/personal-mkdocs/docs/"
-alias nv="vim ${HOME}/workspace/personal-mkdocs/docs/"
+# https://github.com/sharkdp/bat
+if hash bat 2>/dev/null; then
+    alias cat="bat \
+        --theme=ansi-light \
+        --style header,grid \
+        --pager=never \
+        --wrap=never"
+fi
 
+# Notes
+alias n="cd \${HOME}/workspace/personal-mkdocs/docs/"
+alias nv="vim \${HOME}/workspace/personal-mkdocs/docs/"
+
+# Python
 alias venvc='python3 -m virtualenv venv'
+alias ipy="python -m IPython --matplotlib"
