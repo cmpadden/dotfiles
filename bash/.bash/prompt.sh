1cdc40e6a11ce9d4275ec9004b97e067be536fc7
Colton Padden <cp1060@georgetown.edu>
2019-08-15 15:09:26 -0400

    no longer minimal with the git branch in the prompt

--- bash/.bash/prompt.sh
+++ bash/.bash/prompt.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 # https://github.com/direnv/direnv/wiki/Python#restoring-the-ps1
-show_virtual_env() {
+prompt_venv() {
   if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
     echo "($(basename "$VIRTUAL_ENV")) "
   fi
 }
-export -f show_virtual_env
+export -f prompt_venv # required for entering sub-processes
 
-PS1='$(show_virtual_env)'"\w "
+# https://www.shellhacks.com/show-git-branch-terminal-command-prompt/
+prompt_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
+}
+export -f prompt_git_branch # required for entering sub-processes
+
+PS1='$(prompt_venv)''$(prompt_git_branch)'"\w "
