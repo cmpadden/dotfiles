61c7295c1bf588051a0415896cef0252cc3e7634
Colton Padden <cp1060@georgetown.edu>
2020-03-10 14:36:43 -0400

    add colors and utilize helper functions for PS1

--- bash/.bash/prompt.sh
+++ bash/.bash/prompt.sh
@@ -1,17 +1,56 @@
 #!/usr/bin/env bash
 
-# https://github.com/direnv/direnv/wiki/Python#restoring-the-ps1
+###############################################################################
+#                              Helper Functions                               #
+###############################################################################
+
+
+# Get current working virtual environemnt (supporting direnv)
+# Arguments:
+#  None
+# Reference:
+#   https://github.com/direnv/direnv/wiki/Python#restoring-the-ps1
 prompt_venv() {
   if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-    echo "($(basename "$VIRTUAL_ENV")) "
+      printf " %s " "$(basename "$VIRTUAL_ENV")"
   fi
 }
 export -f prompt_venv # required for entering sub-processes
 
-# https://www.shellhacks.com/show-git-branch-terminal-command-prompt/
+# Get current working branch
+# Arguments:
+#   None
+# Reference:
+#   https://www.shellhacks.com/show-git-branch-terminal-command-prompt/
 prompt_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
+  if git rev-parse --git-dir > /dev/null 2>&1; then
+    printf " %s " "$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
+  fi
 }
 export -f prompt_git_branch # required for entering sub-processes
 
-PS1='$(prompt_venv)''$(prompt_git_branch)'"\w "
+# Apply foreground and background colors to string
+# Arguments:
+#  $1 STRING
+#  $2 BACKGROUND_COLOR
+#  $3 FOREGROUND_COLOR
+apply_colors() {
+    # number of arguments will be 2 if string is missing or null
+    if [ $# -eq 3 ]; then
+      printf "\033[48;05;%s;38;05;%sm%s\033[0m" "$2" "$3" "$1"
+    fi
+}
+
+
+###############################################################################
+#                              Tacky-ass Prompt                               #
+###############################################################################
+
+
+# Apply custom colors when using a multiplexor, but not the default shell
+if [ "$TERM" == "screen-256color" ]; then
+  # must be single-quotes for expressions to expand
+  PS1='$(apply_colors "$(prompt_venv)" "214" "0")'
+  PS1+='$(apply_colors "$(prompt_git_branch)" "212" "0")'
+  PS1+='$(apply_colors " $(pwd) \$ " "15" "0")'
+fi
