bc2460f4fde2f3c64c4ba11636504fa465a09bad
colton <colton.padden@fastmail.com>
2022-08-02 20:07:00 -0400

    remove duplicate aliases; add notes and notes_backup

--- bash/.bash/aliases.sh
+++ bash/.bash/aliases.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# Docker
+# docker
 alias drmf='docker rmi -f $(docker images -q)'
 alias drmp='docker kill $(docker ps -q)'
 
-# Git
-alias gs="git status"
+# git
+alias gs="git status ."
 alias gd="git diff"
 alias gu="git shortlog | /usr/bin/grep -E '^[^ ]'"
 alias git_delete_merged="git branch --merged | grep -v 'master' | xargs git branch -d"
 
-# Python
+# python
 alias venvc='python3 -m virtualenv venv'
 alias ipy="python -m IPython --matplotlib"
 alias mkpyenv="echo \"layout python3\" > .envrc && direnv allow"
@@ -19,31 +19,24 @@ alias pydoc='python -m pydoc'
 # Node
 alias npm="pnpm"
 
-# GCP
+# google cloud platform
 alias gcp_project="gcloud info --format='value(config.project)'"
 
-gssh() {
-    gcloud beta compute ssh --zone "us-east4-a" "$1" -- -p 8800
-}
-
-gscp() {
-    gcloud beta compute scp --port 8800 --zone "us-east4-a" "$@"
-}
-
-# Vim
-alias v="vim"
-
+# vim
 if hash nvim 2>/dev/null; then
     alias vim="nvim"
     alias n="nvim"
 fi
+
+# rsync
+alias rscp='rsync -aP'
+alias rsmv='rsync -aP --remove-source-files'
 
 # https://github.com/ogham/exa
 if hash exa 2>/dev/null; then
     alias l='exa -1 --group-directories-first'
     alias ls='exa -l --classify --group --time-style long-iso --group-directories-first'
 else
-    warn "\`exa\` not installed; falling back to \`ls\`"
     alias l='ls -1p'
     alias ls='ls -lhpG'
 fi
@@ -55,26 +48,20 @@ if hash bat 2>/dev/null; then
         --style header,grid \
         --pager=never \
         --wrap=never"
-else
-    warn "\`bat\` not installed; falling back to \`cat\`"
 fi
-
-# Alias Neovim to Vim if installed
-if hash nvim 2>/dev/null; then
-    alias vim="nvim"
-fi
-
-# Python
-alias venvc='python3 -m virtualenv venv'
-alias ipy="python -m IPython --matplotlib"
-
-# Rsync
-alias rscp='rsync -aP'
-alias rsmv='rsync -aP --remove-source-files'
 
 # https://github.com/ggreer/the_silver_searcher
 if hash ag 2>/dev/null; then
     alias grep="ag --hidden --ignore .git --ignore node_modules --ignore dist"
-else
-    echo "\`ag\` not installed -- falling back to \`grep\`"
+fi
+
+# snowflake
+if [ -f /Applications/SnowSQL.app/Contents/MacOS/snowsql ]; then
+    alias snowsql=/Applications/SnowSQL.app/Contents/MacOS/snowsql
+fi
+
+# notes
+if [ -f "$HOME/notes.md.asc" ]; then
+    alias notes="tmux split-window -h 'nvim notes.md.asc'"
+    alias notes_backup='cp "$HOME/notes.md.asc" "$HOME/backups/notes.md.$(date -Iminutes).asc"'
 fi
