113a1551e2c64cb95ab76e50fcbafff4f7a79b50
colton <colton.padden@fastmail.com>
2023-01-04 14:37:40 -0500

    packer.nvim -> lazy.nvim

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -2,89 +2,92 @@
 --                                       Plugins                                       --
 -----------------------------------------------------------------------------------------
 
-local ensure_packer = function()
-  local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+local plugins = {
 
-require('packer').startup(function(use)
-
-    -- NOTE: use `gx` to open a URL in the web browser
-
-    use 'wbthomason/packer.nvim'                       -- https://github.com/wbthomason/packer.nvim'
-
-    use 'editorconfig/editorconfig-vim'                -- https://github.com/editorconfig/editorconfig-vim
-    use 'ggandor/lightspeed.nvim'                      -- https://github.com/ggandor/lightspeed.nvim
-    use { 'iamcco/markdown-preview.nvim',
-          run = 'cd app && yarn install',
-          cmd = 'MarkdownPreview' }                    -- https://github.com/iamcco/markdown-preview.nvim
-    use 'godlygeek/tabular'                            -- https://github.com/godlygeek/tabular
-    use 'joosepalviste/nvim-ts-context-commentstring'  -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
-    use 'jpalardy/vim-slime'                           -- https://github.com/jpalardy/vim-slime
-    use { 'junegunn/fzf',
-          dir = '~/.fzf',
-          run = './install --bin' }                    -- https://github.com/junegunn/fzf
-    use 'ibhagwan/fzf-lua'                             -- https://github.com/ibhagwan/fzf-lua
-    use 'junegunn/goyo.vim'                            -- https://github.com/junegunn/goyo.vim
-    use 'junegunn/vim-easy-align'                      -- https://github.com/junegunn/vim-easy-align
-    use 'lervag/vimtex'                                -- https://github.com/lervag/vimtex
-    use 'lewis6991/gitsigns.nvim'                      -- https://github.com/lewis6991/gitsigns.nvim
-    use 'norcalli/nvim-colorizer.lua'                  -- https://github.com/norcalli/nvim-colorizer.lua
-    use 'nvim-lua/plenary.nvim'                        -- https://github.com/nvim-lua/plenary.nvim
-    use { 'nvim-treesitter/nvim-treesitter',
-          run = ':TSUpdate' }                          -- https://github.com/nvim-treesitter/nvim-treesitter
-    use 'nvim-treesitter/playground'                   -- https://github.com/nvim-treesitter/playground
-    use 'preservim/vim-markdown'                       -- https://github.com/preservim/vim-markdown
-    use 'tpope/vim-commentary'                         -- https://github.com/tpope/vim-commentary
-    use 'tpope/vim-dadbod'                             -- https://github.com/tpope/vim-dadbod
-    use 'tpope/vim-fugitive'                           -- https://github.com/tpope/vim-fugitive
-    use 'tpope/vim-obsession'                          -- https://github.com/tpope/vim-obsession
-    use 'tpope/vim-rhubarb'                            -- https://github.com/tpope/vim-rhubarb
-    use 'tpope/vim-surround'                           -- https://github.com/tpope/vim-surround
-    -- use 'Olical/conjure'
+    -- main
+    { 'editorconfig/editorconfig-vim' },
+    { 'ggandor/lightspeed.nvim' },
+    { 'iamcco/markdown-preview.nvim', build = 'cd app && yarn install', cmd = 'MarkdownPreview' },
+    { 'godlygeek/tabular' },
+    { 'joosepalviste/nvim-ts-context-commentstring' },
+    { 'jpalardy/vim-slime' },
+    { 'junegunn/fzf', dir = '~/.fzf', build = './install --bin' },
+    { 'junegunn/fzf.vim' },
+    { 'ibhagwan/fzf-lua' },
+    { 'junegunn/goyo.vim' },
+    { 'junegunn/vim-easy-align' },
+    { 'lervag/vimtex' },
+    { 'lewis6991/gitsigns.nvim' },
+    { 'norcalli/nvim-colorizer.lua' },
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    { 'nvim-treesitter/playground' },
+    { 'preservim/vim-markdown' },
+    { 'tpope/vim-commentary' },
+    { 'tpope/vim-dadbod' },
+    { 'tpope/vim-fugitive' },
+    { 'tpope/vim-obsession' },
+    { 'tpope/vim-rhubarb' },
+    { 'tpope/vim-surround' },
 
     -- color schemes
-    use { 'folke/tokyonight.nvim',
-          branch = 'main' }                            -- https://github.com/folke/tokyonight.nvim
-    use 'bluz71/vim-moonfly-colors'                    -- https://github.com/bluz71/vim-moonfly-colors
+    {
+      'folke/tokyonight.nvim',
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other start plugins
+      config = function()
+        vim.opt.signcolumn = "yes"
+        vim.opt.termguicolors = true -- support true colors
+        vim.cmd("colorscheme tokyonight-night")
+      end,
+    },
 
     -- lsp
-    use 'williamboman/mason.nvim'                      -- https://github.com/williamboman/mason.nvim
-    use 'williamboman/mason-lspconfig.nvim'            -- https://github.com/williamboman/mason-lspconfig.nvim
-    use 'WhoIsSethDaniel/mason-tool-installer.nvim'    -- https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim
-    use 'neovim/nvim-lspconfig'                        -- https://github.com/neovim/nvim-lspconfig
-    use 'jose-elias-alvarez/null-ls.nvim'              -- https://github.com/jose-elias-alvarez/null-ls.nvim
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
+    { 'neovim/nvim-lspconfig' },
+    { 'jose-elias-alvarez/null-ls.nvim' },
 
     -- completion
-    use 'hrsh7th/nvim-cmp'                             -- https://github.com/hrsh7th/nvim-cmp
-    use 'hrsh7th/cmp-nvim-lsp'                         -- https://github.com/hrsh7th/cmp-nvim-lsp
-    use 'hrsh7th/cmp-buffer'                           -- https://github.com/hrsh7th/cmp-buffer
-    use 'hrsh7th/cmp-path'                             -- https://github.com/hrsh7th/cmp-path
-    use 'hrsh7th/cmp-cmdline'                          -- https://github.com/hrsh7th/cmp-cmdline
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'hrsh7th/cmp-cmdline' },
 
     -- snippets
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
+    { 'L3MON4D3/LuaSnip' },
+    { 'saadparwaiz1/cmp_luasnip' },
 
     -- personal
-    use 'cmpadden/pomodoro.nvim'
+    { 'cmpadden/pomodoro.nvim' },
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+
+}
+
+require("lazy").setup(plugins)
+
 
 -----------------------------------------------------------------------------------------
 --                                Configuration Modules                                --
 -----------------------------------------------------------------------------------------
+
+
+-- TODO: plugins configurations will move into `plugin/` modules as used in lazy.nvim
 
 require('user.globals')
 require('user.options')
