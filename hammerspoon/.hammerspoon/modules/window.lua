b853516019a4898339905ed5e0336c7d7298c707
Colton Padden <colton.padden@fastmail.com>
2023-03-26 14:36:27 -0400

    apply application specific layout on startup given active layout

--- hammerspoon/.hammerspoon/modules/window.lua
+++ hammerspoon/.hammerspoon/modules/window.lua
@@ -1,6 +1,6 @@
--------------------------------------------------------------------------------------------------------------------------
---                                                  Window Management                                                  --
--------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+--                                 Window Management                                  --
+----------------------------------------------------------------------------------------
 
 -- References
 -- https://www.hammerspoon.org/go/#winlayout
@@ -13,11 +13,11 @@ obj.__index = obj
 obj.name = "Window Management"
 obj.version = "1.0.0"
 
--------------------------------------------------------------------------------------------------------------------------
---                                                       Helpers                                                       --
--------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+--                                      Helpers                                       --
+----------------------------------------------------------------------------------------
 
-CENTER_60       = hs.geometry({ x = 0.2, y = 0.0, w = 0.6, h = 1.0 })
+CENTER_60       = hs.geometry({ x = 0.2, y = 0.025, w = 0.6, h = 0.95 })
 CENTER_66       = hs.geometry({ x = 0.1666, y = 0, w = 0.6666, h = 1 })
 LEFT_30         = hs.geometry({ x = 0.0, y = 0.0, w = 0.3, h = 1.0 })
 LEFT_70         = hs.geometry({ x = 0.0, y = 0.0, w = 0.7, h = 1.0 })
@@ -31,76 +31,79 @@ RIGHT_50        = hs.geometry({ x = 0.50, y = 0.0, w = 0.5, h = 1.0 })
 LEFT_30_OFFSET  = hs.geometry({ x = 0.1, y = 0.0, w = 0.4, h = 1.0 })
 RIGHT_50_OFFSET = hs.geometry({ x = 0.5, y = 0.0, w = 0.4, h = 1.0 })
 
--- |[       ]|
-local LAYOUT_PRIMARY = {
-    { "Calendar",      nil, nil, hs.layout.maximized, nil, nil },
-    { "Chromium",      nil, nil, hs.layout.maximized, nil, nil },
-    { "Discord",       nil, nil, hs.layout.maximized, nil, nil },
-    { "Firefox",       nil, nil, hs.layout.maximized, nil, nil },
-    { "Google Chrome", nil, nil, hs.layout.maximized, nil, nil },
-    { "Brave Browser", nil, nil, hs.layout.maximized, nil, nil },
-    { "Logic Pro",     nil, nil, hs.layout.maximized, nil, nil },
-    { "Mail",          nil, nil, hs.layout.maximized, nil, nil },
-    { "Messages",      nil, nil, hs.layout.maximized, nil, nil },
-    { "Notes",         nil, nil, hs.layout.maximized, nil, nil },
-    { "Notion",        nil, nil, hs.layout.maximized, nil, nil },
-    { "Safari",        nil, nil, hs.layout.maximized, nil, nil },
-    { "Slack",         nil, nil, hs.layout.maximized, nil, nil },
-    { "Spotify",       nil, nil, hs.layout.maximized, nil, nil },
-    { "Terminal",      nil, nil, hs.layout.maximized, nil, nil },
-    { "kitty",         nil, nil, hs.layout.maximized, nil, nil },
-    { "zoom.us",       nil, nil, hs.layout.maximized, nil, nil },
-}
+local LAYOUTS = {
 
--- | [  ][  ] |
-local LAYOUT_SECONDARY = {
+    -- Primary layout: |[       ]|
+    [1] = {
+        { "Calendar",      nil, nil, hs.layout.maximized, nil, nil },
+        { "Chromium",      nil, nil, hs.layout.maximized, nil, nil },
+        { "Discord",       nil, nil, hs.layout.maximized, nil, nil },
+        { "Firefox",       nil, nil, hs.layout.maximized, nil, nil },
+        { "Google Chrome", nil, nil, hs.layout.maximized, nil, nil },
+        { "Brave Browser", nil, nil, hs.layout.maximized, nil, nil },
+        { "Logic Pro",     nil, nil, hs.layout.maximized, nil, nil },
+        { "Mail",          nil, nil, hs.layout.maximized, nil, nil },
+        { "Messages",      nil, nil, hs.layout.maximized, nil, nil },
+        { "Notes",         nil, nil, hs.layout.maximized, nil, nil },
+        { "Notion",        nil, nil, hs.layout.maximized, nil, nil },
+        { "Safari",        nil, nil, hs.layout.maximized, nil, nil },
+        { "Slack",         nil, nil, hs.layout.maximized, nil, nil },
+        { "Spotify",       nil, nil, hs.layout.maximized, nil, nil },
+        { "Terminal",      nil, nil, hs.layout.maximized, nil, nil },
+        { "kitty",         nil, nil, hs.layout.maximized, nil, nil },
+        { "zoom.us",       nil, nil, hs.layout.maximized, nil, nil },
+    },
 
-    -- left
-    { "Terminal",      nil, nil, LEFT_30_OFFSET, nil, nil },
-    { "kitty",         nil, nil, LEFT_30_OFFSET, nil, nil },
+    -- Secondary layout: | [  ][  ] |
+    [2] = {
 
-    -- right
-    { "Brave Browser", nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Calendar",      nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Chromium",      nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Discord",       nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Firefox",       nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Google Chrome", nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Logic Pro",     nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Mail",          nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Messages",      nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Notes",         nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Notion",        nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Safari",        nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Slack",         nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "Spotify",       nil, nil, RIGHT_50_OFFSET, nil, nil },
-    { "zoom.us",       nil, nil, RIGHT_50_OFFSET, nil, nil },
-}
+        -- left
+        { "Terminal",      nil, nil, LEFT_30_OFFSET, nil, nil },
+        { "kitty",         nil, nil, LEFT_30_OFFSET, nil, nil },
 
--- |  [   ]  |
-local LAYOUT_CENTERED = {
-    { "Calendar",      nil, nil, CENTER_60, nil, nil },
-    { "Chromium",      nil, nil, CENTER_60, nil, nil },
-    { "Google Chrome", nil, nil, CENTER_60, nil, nil },
-    { "Firefox",       nil, nil, CENTER_60, nil, nil },
-    { "Discord",       nil, nil, CENTER_60, nil, nil },
-    { "Logic Pro",     nil, nil, CENTER_60, nil, nil },
-    { "Mail",          nil, nil, CENTER_60, nil, nil },
-    { "Messages",      nil, nil, CENTER_60, nil, nil },
-    { "Notion",        nil, nil, CENTER_60, nil, nil },
-    { "Notes",         nil, nil, CENTER_60, nil, nil },
-    { "Safari",        nil, nil, CENTER_60, nil, nil },
-    { "Slack",         nil, nil, CENTER_60, nil, nil },
-    { "Spotify",       nil, nil, CENTER_60, nil, nil },
-    { "Terminal",      nil, nil, CENTER_60, nil, nil },
-    { "kitty",         nil, nil, CENTER_60, nil, nil },
-    { "zoom.us",       nil, nil, CENTER_60, nil, nil },
-    { "Figma",         nil, nil, CENTER_60, nil, nil },
-    { "Brave Browser", nil, nil, CENTER_60, nil, nil },
+        -- right
+        { "Brave Browser", nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Calendar",      nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Chromium",      nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Discord",       nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Firefox",       nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Google Chrome", nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Logic Pro",     nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Mail",          nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Messages",      nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Notes",         nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Notion",        nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Safari",        nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Slack",         nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "Spotify",       nil, nil, RIGHT_50_OFFSET, nil, nil },
+        { "zoom.us",       nil, nil, RIGHT_50_OFFSET, nil, nil },
+    },
+
+    -- Centered layout: |  [   ]  |
+    [3] = {
+        { "Calendar",      nil, nil, CENTER_60, nil, nil },
+        { "Chromium",      nil, nil, CENTER_60, nil, nil },
+        { "Google Chrome", nil, nil, CENTER_60, nil, nil },
+        { "Firefox",       nil, nil, CENTER_60, nil, nil },
+        { "Discord",       nil, nil, CENTER_60, nil, nil },
+        { "Logic Pro",     nil, nil, CENTER_60, nil, nil },
+        { "Mail",          nil, nil, CENTER_60, nil, nil },
+        { "Messages",      nil, nil, CENTER_60, nil, nil },
+        { "Notion",        nil, nil, CENTER_60, nil, nil },
+        { "Notes",         nil, nil, CENTER_60, nil, nil },
+        { "Safari",        nil, nil, CENTER_60, nil, nil },
+        { "Slack",         nil, nil, CENTER_60, nil, nil },
+        { "Spotify",       nil, nil, CENTER_60, nil, nil },
+        { "Terminal",      nil, nil, CENTER_60, nil, nil },
+        { "kitty",         nil, nil, CENTER_60, nil, nil },
+        { "zoom.us",       nil, nil, CENTER_60, nil, nil },
+        { "Figma",         nil, nil, CENTER_60, nil, nil },
+        { "Brave Browser", nil, nil, CENTER_60, nil, nil },
+    },
 }
 
 -- multi-display layout; not currently using this
-local function twoDisplayLayout()
+local function two_display_layout()
     local display_primary = hs.screen.allScreens()[1]:name()
     local display_secondary = hs.screen.allScreens()[1]:name()
 
@@ -127,27 +130,27 @@ local function twoDisplayLayout()
     hs.layout.apply(layout)
 end
 
--- enumeration of possible window layouts
-local CENTERED, PRIMARY, SECONDARY = 1, 2, 3
-
--------------------------------------------------------------------------------------------------------------------------
---                                                       Module                                                        --
--------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+--                                       Module                                       --
+----------------------------------------------------------------------------------------
 
 local function application_callback(app_name, event_type, app)
-    if event_type == hs.application.watcher.launched then
-        hs.alert("Launched " .. app_name)
 
+    -- apply layout on application launch
+    if event_type == hs.application.watcher.launched then
+
+        -- workaround: wait for application to be loaded
         while app:mainWindow() == nil do
             -- app:mainWindow() will be `nil` until fully loaded
         end
 
-        if obj.layout == CENTERED then
-            app:mainWindow():moveToUnit(CENTER_66)
-        elseif obj.layout == PRIMARY then
-            app:mainWindow():moveToUnit(RIGHT_70)
-        elseif obj.layout == SECONDARY then
-            app:mainWindow():moveToUnit(CENTER_60)
+        -- apply application-specific layout given current `obj.layout`
+        for _, window in ipairs(LAYOUTS[obj.layout]) do
+            if window[1] == app:name() then
+                hs.alert("[" .. obj.layout .. "] " .. app_name)
+                app:mainWindow():moveToUnit(window[4])
+                break
+            end
         end
     end
 end
@@ -155,27 +158,26 @@ end
 -- Initialize window layout state, and setup application watcher
 function obj:init()
     -- set default window layout
-    self.layout = CENTERED
+    self.layout = 1
 
     -- initialize application watcher to automatically apply layout to new windows
     self.application_watcher = hs.application.watcher.new(application_callback)
     self.application_watcher:start()
     hs.window.animationDuration = 0
 
-
     hs.hotkey.bind({ "cmd", "ctrl" }, "1", function()
-        self.layout = PRIMARY
-        hs.layout.apply(LAYOUT_PRIMARY)
+        self.layout = 1
+        hs.layout.apply(LAYOUTS[self.layout])
     end)
 
     hs.hotkey.bind({ "cmd", "ctrl" }, "2", function()
-        self.layout = SECONDARY
-        hs.layout.apply(LAYOUT_SECONDARY)
+        self.layout = 2
+        hs.layout.apply(LAYOUTS[self.layout])
     end)
 
     hs.hotkey.bind({ "cmd", "ctrl" }, "3", function()
-        self.layout = CENTERED
-        hs.layout.apply(LAYOUT_CENTERED)
+        self.layout = 3
+        hs.layout.apply(LAYOUTS[self.layout])
     end)
 end
 
