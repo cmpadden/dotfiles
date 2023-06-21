4a4c42b2d661aaca259964ceb8831a106ddde146
colton <colton.padden@fastmail.com>
2023-06-21 11:29:59 -0400

    todo: explore nvim-treesitter/nvim-treesitter-textobjects

--- vim/.config/nvim/lua/plugins/treesitter.lua
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,9 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         dependencies = {
-            -- https://github.com/joosepalviste/nvim-ts-context-commentstring
-            { "joosepalviste/nvim-ts-context-commentstring" },
-            -- https://github.com/windwp/nvim-ts-autotag
-            { "windwp/nvim-ts-autotag" },
+            "joosepalviste/nvim-ts-context-commentstring",
+            "windwp/nvim-ts-autotag",
+            -- "nvim-treesitter/nvim-treesitter-textobjects",
         },
         config = function()
             require("nvim-treesitter.configs").setup({
@@ -50,27 +49,26 @@ return {
                 context_commentstring = { enable = true, enable_autocmd = false },
                 highlight = {
                     enable = true,
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
-                    additional_vim_regex_highlighting = false,
-                    -- Disable slow treesitter highlight for large files
-                    disable = function(lang, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        end
+                    disable = function(_, bufnr)
+                        return vim.api.nvim_buf_line_count(bufnr) > 10000
                     end,
                 },
                 incremental_selection = { enable = true },
                 indent = { enable = false },
                 autotag = { enable = true },
+                opts = {
+                    autotag = { enable = true },
+                    context_commentstring = { enable = true, enable_autocmd = false },
+                    highlight = {
+                        enable = true,
+                        disable = function(_, bufnr)
+                            return vim.api.nvim_buf_line_count(bufnr) > 10000
+                        end,
+                    },
+                    incremental_selection = { enable = true },
+                    indent = { enable = true },
+                },
             })
         end,
     },
-
-    -- https://github.com/nvim-treesitter/playground
-    { "nvim-treesitter/playground" },
 }
