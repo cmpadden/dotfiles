dc4708bad91d26d6f7c7588ed23ed8e5f1688374
colton <colton.padden@fastmail.com>
2023-06-23 12:26:07 -0400

    simplify and add conditional checks for lsp bindings

--- vim/.config/nvim/lua/custom/utils/lsp.lua
+++ vim/.config/nvim/lua/custom/utils/lsp.lua
@@ -1,70 +1,72 @@
 local M = {}
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
+-- `on_attach` to map keys after language server attaches to the current buffer
+-- See `:help vim.lsp.*`
+-- See `:help vim.diagnostic.*`
 M.on_attach = function(client, bufnr)
     local opts = { noremap = true, silent = true }
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    -- Global Bindings - Diagnostics
+
+    vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+
+    -- Buffer Bindings
+
+    local opts_buffer = { noremap = true, silent = true, buffer = bufnr }
 
     if client.supports_method("textDocument/declaration") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts_buffer)
     end
 
     if client.supports_method("textDocument/definition") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts_buffer)
     end
 
     if client.supports_method("textDocument/hover") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts_buffer)
     end
 
     if client.supports_method("textDocument/implementation") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts_buffer)
     end
 
     if client.supports_method("textDocument/signatureHelp") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts_buffer)
     end
 
     if client.supports_method("textDocument/typeDefinition") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts_buffer)
     end
 
     if client.supports_method("textDocument/rename") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts_buffer)
     end
 
     if client.supports_method("textDocument/codeAction") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+        vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts_buffer)
     end
 
     if client.supports_method("textDocument/references") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts_buffer)
     end
 
     if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", opts)
+        vim.keymap.set("n", "<space>f", function()
+            vim.lsp.buf.format({ async = true })
+        end, opts_buffer)
     end
 
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    -- if client.supports_method("textDocument/workspaceEdit") then
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts_buffer)
 
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts_buffer)
 
-    vim.api.nvim_buf_set_keymap(
-        bufnr,
-        "n",
-        "<space>wl",
-        "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-        opts
-    )
-
-    -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-
-    vim.api.nvim_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    vim.api.nvim_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-    vim.api.nvim_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-    vim.api.nvim_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+    vim.keymap.set("n", "<space>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts_buffer)
 
     -- TODO - review if needed
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/issues/1131#issuecomment-1432408485
