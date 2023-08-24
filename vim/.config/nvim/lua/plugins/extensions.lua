689246c00c576c31374d5adf867083a3413b9cce
colton <colton.padden@fastmail.com>
2023-08-24 13:12:28 -0400

    use slime bracketed paste

--- vim/.config/nvim/lua/plugins/extensions.lua
+++ vim/.config/nvim/lua/plugins/extensions.lua
@@ -43,6 +43,7 @@ return {
             vim.g.slime_target = "tmux"
             vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
             vim.g.slime_dont_ask_default = 1
+            vim.g.slime_bracketed_paste = 1 -- https://github.com/jpalardy/vim-slime#tmux
         end,
     },
 
