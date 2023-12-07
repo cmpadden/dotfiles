57b014351bf190efb9ccf0bcc317dbd0cfe55dc9
colton <colton.padden@fastmail.com>
2023-12-07 12:50:15 -0500

    back to modified catppuccin

--- vim/.config/nvim/lua/plugins/colors.lua
+++ vim/.config/nvim/lua/plugins/colors.lua
@@ -4,48 +4,37 @@
 
 return {
 
-    -- -- https://github.com/folke/tokyonight.nvim
-    -- {
-    --     "folke/tokyonight.nvim",
-    --     lazy = false,
-    --     priority = 1000,
-    --     init = function()
-    --         vim.cmd("colorscheme tokyonight-night")
-    --     end,
-    -- },
-
     -- Error: `invalid node type at position 2765 for language vim`
     -- Solution:
     -- > rm /opt/homebrew/lib/nvim/parser/vim.so
 
-    {
-        "EdenEast/nightfox.nvim",
-        lazy = false,
-        priority = 1000,
-        init = function()
-            -- vim.cmd.colorscheme("nordfox")
-            vim.cmd.colorscheme("terafox")
-            -- vim.cmd.colorscheme("dayfox")
-            -- vim.cmd.colorscheme("dawnfox")
-        end,
-    },
-
+    -- -- https://github.com/EdenEast/nightfox.nvim
     -- {
-    --     "catppuccin/nvim",
-    --     name = "catppuccin",
+    --     "EdenEast/nightfox.nvim",
+    --     lazy = false,
     --     priority = 1000,
     --     init = function()
-    --         require("catppuccin").setup({
-    --             flavour = "mocha", -- latte, frappe, macchiato, mocha
-    --             term_colors = true,
-    --             transparent_background = false,
-    --             color_overrides = {
-    --                 mocha = {
-    --                     base = "#000000",
-    --                 },
-    --             },
-    --         })
-    --         vim.cmd.colorscheme("catppuccin")
+    --         vim.cmd.colorscheme("terafox")
     --     end,
     -- },
+
+    -- https://github.com/catppuccin/nvim
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        init = function()
+            require("catppuccin").setup({
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                term_colors = true,
+                transparent_background = false,
+                color_overrides = {
+                    mocha = {
+                        base = "#000000",
+                    },
+                },
+            })
+            vim.cmd.colorscheme("catppuccin")
+        end,
+    },
 }
