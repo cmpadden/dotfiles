5dd13df4ed1398d4b43463af2e41272937703c85
Colton Padden <colton.padden@fastmail.com>
2024-03-25 17:11:16 -0400

    [bash] drastically simplify PS1

--- bash/.bash/prompt.sh
+++ bash/.bash/prompt.sh
@@ -1,74 +1,42 @@
 #!/usr/bin/env bash
 
-###############################################################################
-#                              Helper Functions                               #
-###############################################################################
-
-# Get current working virtual environemnt (supporting direnv)
-# Arguments:
-#  None
-# Reference:
+#
+# Gets current working Python virtual environemnt including direnv.
+#
+# Reference(s):
 #   https://github.com/direnv/direnv/wiki/Python#restoring-the-ps1
-prompt_venv() {
+#
+prompt_venv()
+{
     if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-        printf "%s" "$(basename "$VIRTUAL_ENV")"
+        printf "%s " "$(basename "$VIRTUAL_ENV")"
     fi
 }
-export -f prompt_venv # required for entering sub-processes
 
-# Get current working branch
-# Arguments:
-#   None
-# Reference:
+#
+# Gets current working Git branch.
+#
+# Reference(s):
 #   https://www.shellhacks.com/show-git-branch-terminal-command-prompt/
-prompt_git_branch() {
+#
+prompt_git_branch()
+{
     if git rev-parse --git-dir >/dev/null 2>&1; then
-        printf "%s" "$(git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
-    fi
-}
-export -f prompt_git_branch # required for entering sub-processes
-
-# Apply foreground and background colors to string
-# Arguments:
-#  $1 STRING
-#  $2 BACKGROUND_COLOR
-#  $3 FOREGROUND_COLOR
-apply_colors() {
-    # requires three arguments
-    if [ $# -eq 3 ]; then
-        if [ -z "$1" ]; then
-            printf ""
-        else
-            printf "\033[48;05;%s;38;05;%sm %s \033[0m" "$2" "$3" "$1"
-        fi
+        printf "%s " "$(git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')"
     fi
 }
 
-###############################################################################
-#                                     PS1                                     #
-###############################################################################
-
-venv_bg="232"
-venv_fg="255"
-
-# git_bg="255"
-# git_bg="16"
-git_bg="10"
-git_fg="232"
-
-bg="232"
-fg="255"
-
-
-# &#x276E; ❭
-# &#x276F; ❯
-# &#x2771; ❱
-
-# Apply custom colors when using a multiplexor, but not the default shell
+# Applies custom prompt when `xterm-256color` is active (eg. when in tmux)
+#
+# \e[0;32m\]
+# 0 -> foreground (1: foreground bold; 4: foreground underlined, nil: background)
+# 2 -> color code
+#
+# Reference(s):
+#  - https://wiki.gentoo.org/wiki/Bash#PS1 for more information.
+#  - https://wiki.archlinux.org/title/Bash/Prompt_customization
+#
 if [ "$TERM" == "xterm-256color" ]; then
-    # must be single-quotes for expressions to expand
-    PS1='$(apply_colors "$(prompt_venv)" "$venv_bg" "$venv_fg")'
-    PS1+='$(apply_colors "$(prompt_git_branch)" "$git_bg" "$git_fg")'
-    PS1+='$(apply_colors "$(pwd)" "$bg" "$fg")'
-    PS1+='\n ❯ '
+    # PS1='\e[0;32m\]$(prompt_venv)\e[0;33m\]$(prompt_git_branch)\e[0;32m\]\w\n\e[0m\] ❯ '
+    PS1='\e[0;32m\]$(prompt_venv)\e[0;33m\]$(prompt_git_branch)\e[0;32m\]\w\n\e[0m\]\$ '
 fi
