1559b6e9e3cfd273917e5e1f23465f5bf0c9d161
Colton Padden <colton.padden@fastmail.com>
2024-03-31 19:53:33 -0400

    [hammerspoon] disable file monitor watcher

--- hammerspoon/.hammerspoon/modules/watchers.lua
+++ hammerspoon/.hammerspoon/modules/watchers.lua
@@ -1,19 +1,19 @@
 local helpers = require("modules.helpers")
 
-local function callback_pathwatcher(files, flagTables)
-    local mods = {}
-    for ix, file in pairs(files) do
-        local events = {}
-        for event, _ in pairs(flagTables[ix]) do
-            events[#events + 1] = event
-        end
-        mods[#mods + 1] = {
-            name = table.concat(events, ","),
-            value = file,
-        }
-    end
-    helpers:show("Downloads Updated", mods)
-end
+-- local function callback_pathwatcher(files, flagTables)
+--     local mods = {}
+--     for ix, file in pairs(files) do
+--         local events = {}
+--         for event, _ in pairs(flagTables[ix]) do
+--             events[#events + 1] = event
+--         end
+--         mods[#mods + 1] = {
+--             name = table.concat(events, ","),
+--             value = file,
+--         }
+--     end
+--     helpers:show("Downloads Updated", mods)
+-- end
 
-DOWNLOAD_WATCHER = hs.pathwatcher.new(os.getenv("HOME") .. "/Downloads/", callback_pathwatcher)
-DOWNLOAD_WATCHER:start()
+-- DOWNLOAD_WATCHER = hs.pathwatcher.new(os.getenv("HOME") .. "/Downloads/", callback_pathwatcher)
+-- DOWNLOAD_WATCHER:start()
