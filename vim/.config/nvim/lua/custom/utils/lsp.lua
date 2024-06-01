5989fa6cd1c9ccd1677917e8745bf4839dadb571
Colton Padden <colton.padden@fastmail.com>
2024-06-01 18:24:55 -0400

    [vim] show diagnostics popup on <leader>d

--- vim/.config/nvim/lua/custom/utils/lsp.lua
+++ vim/.config/nvim/lua/custom/utils/lsp.lua
@@ -15,7 +15,7 @@ M.on_attach = function(client, bufnr)
 
     -- Global Bindings - Diagnostics
 
-    vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "<space>d", vim.diagnostic.open_float, opts)
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
     vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
