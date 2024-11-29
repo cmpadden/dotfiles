949059776e511b78a5433b5969dc4ba85ecc34b8
Colton Padden <colton.padden@fastmail.com>
2024-11-29 17:01:35 -0500

    [misc] standardize defaults checks / writes

--- _configure.sh
+++ _configure.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
+#
+# REFERENCES
+#
+#     https://macos-defaults.com/dock/autohide.html
+#
 
-if [ "$OS_NAME" = 'Darwin' ]; then
-    echo "Setting macOS defaults InitialKeyRepeat, KeyRepeat"
-    defaults write -g InitialKeyRepeat -int 10
-    defaults write -g KeyRepeat -int 1
+set -e
+set -x
 
-    # https://macos-defaults.com/dock/autohide.html
-    echo "Setting macOS defaults for Dock and Menu Bar"
+if [[ "$OSTYPE" = 'darwin'* ]]; then
+
+    if [ ! '10' = $(defaults read -g InitialKeyRepeat) ]; then
+        defaults write -g InitialKeyRepeat -int 10
+    fi
+
+    if [ ! '1' = $(defaults read -g KeyRepeat) ]; then
+        defaults write -g KeyRepeat -int 1
+    fi
 
     # automatically hide dock
-    defaults write com.apple.dock "autohide" -bool "true"
+    if [ '0' = $(defaults read com.apple.dock autohide) ]; then
+        defaults write com.apple.dock autohide -bool true
+    fi
 
     # disable dock animation
-    defaults write com.apple.dock "autohide-time-modifier" -float "0"
+    if [ ! '0' = $(defaults read com.apple.dock autohide-time-modifier) ]; then
+        defaults write com.apple.dock autohide-time-modifier -float "0"
+    fi
 
     # resize dock (default size: 42)
-    defaults write com.apple.dock tilesize -int 35
-
-    # restart dock
-    killall Dock
+    if [ ! '35' = $(defaults read com.apple.dock tilesize) ]; then
+        defaults write com.apple.dock tilesize -int 35
+    fi
 
     # https://github.com/yannbertrand/macos-defaults/issues/268
-    defaults write NSGlobalDomain _HIHideMenuBar -bool true
+    if [ '0' = $(defaults read NSGlobalDomain _HIHideMenuBar) ]; then
+        defaults write NSGlobalDomain _HIHideMenuBar -bool true
+    fi
 
     # Prevent "Click on Desktop to reveal wallpaper"
-    defaults write com.apple.finder CreateDesktop -bool false
-    killall Finder
+    if [ '1' = $(defaults read com.apple.finder CreateDesktop) ]; then
+        defaults write com.apple.finder CreateDesktop -bool false
+    fi
 
     if ! /usr/bin/grep -q '/opt/homebrew/bin/bash' /etc/shells; then
         echo 'Adding /opt/homebrew/bin/bash to /etc/shells'
@@ -37,4 +53,8 @@ if [ "$OS_NAME" = 'Darwin' ]; then
         echo "/opt/homebrew/bin/bash" | sudo tee -a /etc/shells
         chsh -s /opt/homebrew/bin/bash
     fi
+
+    # TODO - use conditional callback/finally
+    killall Dock
+    killall Finder
 fi
