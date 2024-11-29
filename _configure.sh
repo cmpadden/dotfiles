aca634f1204778925f2c0551731d4a169d28b3d9
Colton Padden <colton.padden@fastmail.com>
2024-11-29 17:07:51 -0500

    [misc] default touch bar to function keys

--- _configure.sh
+++ _configure.sh
@@ -8,53 +8,60 @@
 set -e
 set -x
 
-if [[ "$OSTYPE" = 'darwin'* ]]; then
-
-    if [ ! '10' = $(defaults read -g InitialKeyRepeat) ]; then
-        defaults write -g InitialKeyRepeat -int 10
-    fi
-
-    if [ ! '1' = $(defaults read -g KeyRepeat) ]; then
-        defaults write -g KeyRepeat -int 1
-    fi
-
-    # automatically hide dock
-    if [ '0' = $(defaults read com.apple.dock autohide) ]; then
-        defaults write com.apple.dock autohide -bool true
-    fi
-
-    # disable dock animation
-    if [ ! '0' = $(defaults read com.apple.dock autohide-time-modifier) ]; then
-        defaults write com.apple.dock autohide-time-modifier -float "0"
-    fi
-
-    # resize dock (default size: 42)
-    if [ ! '35' = $(defaults read com.apple.dock tilesize) ]; then
-        defaults write com.apple.dock tilesize -int 35
-    fi
-
-    # https://github.com/yannbertrand/macos-defaults/issues/268
-    if [ '0' = $(defaults read NSGlobalDomain _HIHideMenuBar) ]; then
-        defaults write NSGlobalDomain _HIHideMenuBar -bool true
-    fi
-
-    # Prevent "Click on Desktop to reveal wallpaper"
-    if [ '1' = $(defaults read com.apple.finder CreateDesktop) ]; then
-        defaults write com.apple.finder CreateDesktop -bool false
-    fi
-
-    if ! /usr/bin/grep -q '/opt/homebrew/bin/bash' /etc/shells; then
-        echo 'Adding /opt/homebrew/bin/bash to /etc/shells'
-        echo "/opt/homebrew/bin/bash" >> /etc/shells
-    fi
-
-    if [ ! "$SHELL" = "/opt/homebrew/bin/bash" ]; then
-        echo 'Changing shell to /opt/homebrew/bin/bash'
-        echo "/opt/homebrew/bin/bash" | sudo tee -a /etc/shells
-        chsh -s /opt/homebrew/bin/bash
-    fi
-
-    # TODO - use conditional callback/finally
-    killall Dock
-    killall Finder
+if [[ ! "$OSTYPE" = 'darwin'* ]]; then
+    exit
 fi
+
+if [ ! '10' = $(defaults read -g InitialKeyRepeat) ]; then
+    defaults write -g InitialKeyRepeat -int 10
+fi
+
+if [ ! '1' = $(defaults read -g KeyRepeat) ]; then
+    defaults write -g KeyRepeat -int 1
+fi
+
+# automatically hide dock
+if [ '0' = $(defaults read com.apple.dock autohide) ]; then
+    defaults write com.apple.dock autohide -bool true
+fi
+
+# disable dock animation
+if [ ! '0' = $(defaults read com.apple.dock autohide-time-modifier) ]; then
+    defaults write com.apple.dock autohide-time-modifier -float "0"
+fi
+
+# resize dock (default size: 42)
+if [ ! '35' = $(defaults read com.apple.dock tilesize) ]; then
+    defaults write com.apple.dock tilesize -int 35
+fi
+
+# https://github.com/yannbertrand/macos-defaults/issues/268
+if [ '0' = $(defaults read NSGlobalDomain _HIHideMenuBar) ]; then
+    defaults write NSGlobalDomain _HIHideMenuBar -bool true
+fi
+
+# Prevent "Click on Desktop to reveal wallpaper"
+if [ '1' = $(defaults read com.apple.finder CreateDesktop) ]; then
+    defaults write com.apple.finder CreateDesktop -bool false
+fi
+
+if ! /usr/bin/grep -q '/opt/homebrew/bin/bash' /etc/shells; then
+    echo 'Adding /opt/homebrew/bin/bash to /etc/shells'
+    echo "/opt/homebrew/bin/bash" >> /etc/shells
+fi
+
+# default to show function keys in touch bar
+if [ ! "functionKeys" = $(defaults read com.apple.touchbar.agent PresentationModeGlobal) ]; then
+    defaults write com.apple.touchbar.agent PresentationModeGlobal functionKeys
+    pkill "Touch Bar agent"; killall "ControlStrip";
+fi
+
+if [ ! "$SHELL" = "/opt/homebrew/bin/bash" ]; then
+    echo 'Changing shell to /opt/homebrew/bin/bash'
+    echo "/opt/homebrew/bin/bash" | sudo tee -a /etc/shells
+    chsh -s /opt/homebrew/bin/bash
+fi
+
+# TODO - use conditional callback/finally
+killall Dock
+killall Finder
