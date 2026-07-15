dc9e11008285bcde8a558bfd993c1236d011e1a3
Colton Padden <colton.padden@fastmail.com>
2026-07-15 16:12:50 -0400

    [neovim] remote antiquated / unused tree-sitter extensions

--- vim/.config/nvim/lua/plugins/treesitter.lua
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,187 +1,42 @@
+local ensure_installed = {
+    "bash",
+    "css",
+    "diff",
+    "gitignore",
+    "hcl",
+    "html",
+    "http",
+    "javascript",
+    "jsdoc",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "rust",
+    "sql",
+    "toml",
+    "typescript",
+    "vue",
+    "yaml",
+}
+
 return {
     -- https://github.com/nvim-treesitter/nvim-treesitter
     {
         "nvim-treesitter/nvim-treesitter",
-        dependencies = {
-            {
-                "joosepalviste/nvim-ts-context-commentstring",
-                init = function()
-                    -- Disable the legacy nvim-treesitter module before this plugin loads.
-                    vim.g.skip_ts_context_commentstring_module = true
-                end,
-                opts = {
-                    enable_autocmd = false,
-                },
-                config = function(_, opts)
-                    require("ts_context_commentstring").setup(opts)
+        lazy = false,
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter").install(ensure_installed)
 
-                    local original_get_option = vim.filetype._get_option
-                        or vim.filetype.get_option
-                    vim.filetype._get_option = original_get_option
-
-                    vim.filetype.get_option = function(filetype, option)
-                        if option ~= "commentstring" then
-                            return original_get_option(filetype, option)
-                        end
-
-                        return require("ts_context_commentstring.internal").calculate_commentstring()
-                            or original_get_option(filetype, option)
+            local group = vim.api.nvim_create_augroup("user_treesitter", { clear = true })
+            vim.api.nvim_create_autocmd("FileType", {
+                group = group,
+                callback = function(args)
+                    if vim.api.nvim_buf_line_count(args.buf) <= 10000 then
+                        pcall(vim.treesitter.start, args.buf)
                     end
                 end,
-            },
-            "windwp/nvim-ts-autotag",
-            {
-                "nvim-treesitter/nvim-treesitter-textobjects",
-                branch = "main",
-            },
-        },
-        event = { "BufReadPost", "BufNewFile" },
-        cmd = { "TSUpdate", "TSUpdateSync" },
-        build = ":TSUpdateSync",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                enable_autocmd = false,
-            })
-
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "bash",
-                    "css",
-                    "diff",
-                    "gitignore",
-                    "hcl",
-                    "html",
-                    "http",
-                    "javascript",
-                    "jsdoc",
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "python",
-                    "rust",
-                    "sql",
-                    "toml",
-                    "typescript",
-                    "vue",
-                    "yaml",
-                },
-                highlight = {
-                    enable = true,
-                    disable = function(_, bufnr)
-                        return vim.api.nvim_buf_line_count(bufnr) > 10000
-                    end,
-                },
-                incremental_selection = { enable = true },
-                indent = { enable = false },
-                autotag = { enable = true },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true,
-                        keymaps = {
-                            ["ak"] = "@block.outer",
-                            ["ik"] = "@block.inner",
-                            ["ac"] = "@class.outer",
-                            ["ic"] = "@class.inner",
-                            ["a?"] = "@conditional.outer",
-                            ["i?"] = "@conditional.inner",
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["al"] = "@loop.outer",
-                            ["il"] = "@loop.inner",
-                            ["aa"] = "@parameter.outer",
-                            ["ia"] = "@parameter.inner",
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true,
-                        goto_next_start = {
-                            ["]k"] = {
-                                query = "@block.outer",
-                                desc = "Next block start",
-                            },
-                            ["]f"] = {
-                                query = "@function.outer",
-                                desc = "Next function start",
-                            },
-                            ["]a"] = {
-                                query = "@parameter.outer",
-                                desc = "Next parameter start",
-                            },
-                        },
-                        goto_next_end = {
-                            ["]k"] = { query = "@block.outer", desc = "Next block end" },
-                            ["]f"] = {
-                                query = "@function.outer",
-                                desc = "Next function end",
-                            },
-                            ["]a"] = {
-                                query = "@parameter.outer",
-                                desc = "Next parameter end",
-                            },
-                        },
-                        goto_previous_start = {
-                            ["[k"] = {
-                                query = "@block.outer",
-                                desc = "Previous block start",
-                            },
-                            ["[f"] = {
-                                query = "@function.outer",
-                                desc = "Previous function start",
-                            },
-                            ["[a"] = {
-                                query = "@parameter.outer",
-                                desc = "Previous parameter start",
-                            },
-                        },
-                        goto_previous_end = {
-                            ["[K"] = {
-                                query = "@block.outer",
-                                desc = "Previous block end",
-                            },
-                            ["[F"] = {
-                                query = "@function.outer",
-                                desc = "Previous function end",
-                            },
-                            ["[A"] = {
-                                query = "@parameter.outer",
-                                desc = "Previous parameter end",
-                            },
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            [">K"] = {
-                                query = "@block.outer",
-                                desc = "Swap next block",
-                            },
-                            [">F"] = {
-                                query = "@function.outer",
-                                desc = "Swap next function",
-                            },
-                            [">A"] = {
-                                query = "@parameter.inner",
-                                desc = "Swap next parameter",
-                            },
-                        },
-                        swap_previous = {
-                            ["<K"] = {
-                                query = "@block.outer",
-                                desc = "Swap previous block",
-                            },
-                            ["<F"] = {
-                                query = "@function.outer",
-                                desc = "Swap previous function",
-                            },
-                            ["<A"] = {
-                                query = "@parameter.inner",
-                                desc = "Swap previous parameter",
-                            },
-                        },
-                    },
-                },
             })
 
             -- Neovim 0.12 is still tripping over markdown injection parsing in this config.
